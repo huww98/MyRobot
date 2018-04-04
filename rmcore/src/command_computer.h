8800a67d512785cf05725a40654fe723a2aa3231
胡玮文 <huww98@outlook.com>
2018-04-05 00:55:19 +0800

    初步实现command_computer

--- rmcore/src/command_computer.h
+++ rmcore/src/command_computer.h
@@ -1,7 +1,15 @@
 #include <teb_local_planner/optimal_planner.h>
 #include <ros/ros.h>
-
+#include <vector>
 #include "common_types.h"
+
+struct Segment
+{
+    int Id;
+    std::vector<Eigen::Vector2d> ViaPoints;
+    std::vector<teb_local_planner::PoseSE2> InitialPlanPoses;
+    std::vector<double> InitialPlanDt;
+};
 
 class CommandComputer
 {
@@ -11,6 +19,14 @@ class CommandComputer
     AccelerationCommand ComputeCommand(const RobotState &state);
 
   private:
+    void loadSegments();
+    void loadConfig(ros::NodeHandle nh);
+    void clearOldViaPoint();
+
+    std::vector<Segment> allSegments;
+    std::vector<Segment>::iterator currentOnSegment = allSegments.begin();
+    std::vector<Segment>::iterator nextSegment = allSegments.begin();
+
     teb_local_planner::TebConfig cfg;
     teb_local_planner::TebOptimalPlanner planner;
     teb_local_planner::ViaPointContainer viaPoints;
